2555091381bf5e741094e90680d2d2358c0465d9
Ekansh <zethyst@protonmail.com>
2023-06-01 19:55:02 +0530

    Made some improvements

--- quesList.js
+++ quesList.js
@@ -31,93 +31,93 @@ const quizArr = [
         opt4:"Blue & white",
         answer:"Red & white"
     },
-    // {
-    //     question:"Who is Batman's crime-fighting assistant?",
-    //     opt1:"Selena",
-    //     opt2:"Robin",
-    //     opt3:"Harvey",
-    //     opt4:"Gordan",
-    //     answer:"Robin"
-    // },
-    // {
-    //     question:"Which fictional character was raised by apes?",
-    //     opt1:"Mowgli",
-    //     opt2:"Tarzan",
-    //     opt3:"Harry Potter",
-    //     opt4:"You",
-    //     answer:"Tarzan"
-    // },
-    // {
-    //     question:"The racquet of which of these games does not have a net or strings in it?",
-    //     opt1:"Squash",
-    //     opt2:"Tennis",
-    //     opt3:"Badminton",
-    //     opt4:"Table Tennis",
-    //     answer:"Table Tennis"
-    // },
-    // {
-    //     question:"Which of these legendary characters released a genie by rubbing a magical lamp?",
-    //     opt1:"King Arthur",
-    //     opt2:"Hercules",
-    //     opt3:"Pocahontas",
-    //     opt4:"Aladdin",
-    //     answer:"Aladdin"
-    // },
-    // {
-    //     question:"Which of these ancient peoples are known for mummifying bodies?",
-    //     opt1:"Romans",
-    //     opt2:"Vikings",
-    //     opt3:"Egyptians",
-    //     opt4:"Greeks",
-    //     answer:"Egyptians"
-    // },
-    // {
-    //     question:"In the world of computers and technology, what does the H in HTTP stand for?",
-    //     opt1:"High-Definition",
-    //     opt2:"Hyper",
-    //     opt3:"Heightened",
-    //     opt4:"Hypervisor",
-    //     answer:"Hyper"
-    // },
-    // {
-    //     question:"Which term is used to describe a very good investment?",
-    //     opt1:"Potato Chip",
-    //     opt2:"Green Chip",
-    //     opt3:"Blue Chip",
-    //     opt4:"Wood Chip",
-    //     answer:"Blue Chip"
-    // },
-    // {
-    //     question:"What object launched by NASA was named after Edwin Hubble?",
-    //     opt1:"Satellite",
-    //     opt2:"Space Telescope",
-    //     opt3:"Space Shuttle",
-    //     opt4:"Image Sensor",
-    //     answer:"Space Telescope"
-    // },
-    // {
-    //     question:"Which of these alcoholic drinks originated in Mexico?",
-    //     opt1:"Tequila",
-    //     opt2:"Martini",
-    //     opt3:"Sake",
-    //     opt4:"Bloody Mary",
-    //     answer:"Tequila"
-    // },
-    // {
-    //     question:"After which Greek Goddess is the US space program to land the first woman and next man on the Moon by 2024 named?",
-    //     opt1:"Rhea",
-    //     opt2:"Nemesis",
-    //     opt3:"Aphrodite",
-    //     opt4:"Artemis",
-    //     answer:"Artemis"
-    // },
-    // {
-    //     question:"In 2019, which telescope captured the first-ever Black Hole image?",
-    //     opt1:"James Webb Space Telescope",
-    //     opt2:"Event Horizon Telescope",
-    //     opt3:"Hubble Space Telescope",
-    //     opt4:"Spitzer Space Telescope",
-    //     answer:"Event Horizon Telescope"
-    // }
+    {
+        question:"Who is Batman's crime-fighting assistant?",
+        opt1:"Selena",
+        opt2:"Robin",
+        opt3:"Harvey",
+        opt4:"Gordan",
+        answer:"Robin"
+    },
+    {
+        question:"Which fictional character was raised by apes?",
+        opt1:"Mowgli",
+        opt2:"Tarzan",
+        opt3:"Harry Potter",
+        opt4:"You",
+        answer:"Tarzan"
+    },
+    {
+        question:"The racquet of which of these games does not have a net or strings in it?",
+        opt1:"Squash",
+        opt2:"Tennis",
+        opt3:"Badminton",
+        opt4:"Table Tennis",
+        answer:"Table Tennis"
+    },
+    {
+        question:"Which of these legendary characters released a genie by rubbing a magical lamp?",
+        opt1:"King Arthur",
+        opt2:"Hercules",
+        opt3:"Pocahontas",
+        opt4:"Aladdin",
+        answer:"Aladdin"
+    },
+    {
+        question:"Which of these ancient peoples are known for mummifying bodies?",
+        opt1:"Romans",
+        opt2:"Vikings",
+        opt3:"Egyptians",
+        opt4:"Greeks",
+        answer:"Egyptians"
+    },
+    {
+        question:"In the world of computers and technology, what does the H in HTTP stand for?",
+        opt1:"High-Definition",
+        opt2:"Hyper",
+        opt3:"Heightened",
+        opt4:"Hypervisor",
+        answer:"Hyper"
+    },
+    {
+        question:"Which term is used to describe a very good investment?",
+        opt1:"Potato Chip",
+        opt2:"Green Chip",
+        opt3:"Blue Chip",
+        opt4:"Wood Chip",
+        answer:"Blue Chip"
+    },
+    {
+        question:"What object launched by NASA was named after Edwin Hubble?",
+        opt1:"Satellite",
+        opt2:"Space Telescope",
+        opt3:"Space Shuttle",
+        opt4:"Image Sensor",
+        answer:"Space Telescope"
+    },
+    {
+        question:"Which of these alcoholic drinks originated in Mexico?",
+        opt1:"Tequila",
+        opt2:"Martini",
+        opt3:"Sake",
+        opt4:"Bloody Mary",
+        answer:"Tequila"
+    },
+    {
+        question:"After which Greek Goddess is the US space program to land the first woman and next man on the Moon by 2024 named?",
+        opt1:"Rhea",
+        opt2:"Nemesis",
+        opt3:"Aphrodite",
+        opt4:"Artemis",
+        answer:"Artemis"
+    },
+    {
+        question:"In 2019, which telescope captured the first-ever Black Hole image?",
+        opt1:"James Webb Space Telescope",
+        opt2:"Event Horizon Telescope",
+        opt3:"Hubble Space Telescope",
+        opt4:"Spitzer Space Telescope",
+        answer:"Event Horizon Telescope"
+    }
 
 ];
